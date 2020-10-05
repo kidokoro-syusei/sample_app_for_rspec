448eb966b3fe6ae4727890522f36af5c7ef5efb7
kidokoro-syusei <kezu20@gmail.com>
2020-10-05 13:14:39 +0900

    Fix:add_tsak_spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -11,15 +11,21 @@ RSpec.describe Task, type: :model do
       expect(task.errors[:title]).to_not include("を入力してください")
     end
     it 'タイトルはユニークであること' do
-      FactoryBot.create(:task, title: "kidokoro")
-      task = FactoryBot.build(:task, title: "kidokoro")
-      task.valid?
-      expect(task.errors[:title].to include("はすでに存在しています"))
-    end
+      task = create(:task)
+      task_with_duplicated_title = build(:task, title: task.title)
+      expect(task_with_duplicated_title).to be_invalid
+      expect(task_with_duplicated_title.errors[:title]).to eq ["has already been taken"]
+    end  
     it 'ステータスが必須項目であること' do
       task = Task.new(status: nil)
       task.valid?
       expect(task.errors[:status]).to_not include("を入力してください")
     end
+    it 'is valid with all attributes' do
+      task = build(:task)
+      expect(task).to be_valid
+      expect(task.errors).to be_empty
+    end
+
   end
 end
