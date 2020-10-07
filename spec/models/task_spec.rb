2c50d17a9b0e43fed1e1689ec714e503f9c80c87
kidokoro-syusei <kezu20@gmail.com>
2020-10-07 09:26:02 +0900

    Fix:task_spec_fixed

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,30 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'validation' do
-    it "has a valid factory" do
-      expect(FactoryBot.build(:task)).to be_valid
-    end
-    it 'タイトルが必須項目であること' do
-      task = FactoryBot.build(:task, title: nil)
-      task.valid?
-      expect(task.errors[:title]).to_not include("を入力してください")
-    end
-    it 'タイトルはユニークであること' do
-      task = create(:task)
-      task_with_duplicated_title = build(:task, title: task.title)
-      expect(task_with_duplicated_title).to be_invalid
-      expect(task_with_duplicated_title.errors[:title]).to eq ["has already been taken"]
-    end  
-    it 'ステータスが必須項目であること' do
-      task = Task.new(status: nil)
-      task.valid?
-      expect(task.errors[:status]).to_not include("を入力してください")
-    end
     it 'is valid with all attributes' do
       task = build(:task)
       expect(task).to be_valid
       expect(task.errors).to be_empty
     end
-
+    it 'タイトルが必須項目であること' do
+      task_without_title = FactoryBot.build(:task, title: nil)
+      expect(task_without_title).to be_invalid
+      expect(task_without_title.errors[:title]).to eq ["を入力してください"]
+    end
+    it '同じタイトルが存在しないこと' do
+      task = create(:task)
+      task_with_duplicated_title = build(:task, title: task.title)
+      expect(task_with_duplicated_title).to be_invalid
+      expect(task_with_duplicated_title.errors[:title]).to eq ["はすでに存在してます"]
+    end  
+    it 'ステータスが必須項目であること' do
+      task = Task.new(status: nil)
+      task_without_status.to be_invalid
+      expect(task_without_status.errors[:status]).to_not eq ["を入力してください"]
+    end
+    it 'タイトルは違う名前であること' do
+      task = create(:task)
+      expect(task).to be_valid
+      expect(task.errors).to be_empty
+    end
   end
 end
